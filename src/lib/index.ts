6feafcb8960141aa9511856b6bca8668e2aa184b
Orwellian <orwellian225@gmail.com>
2023-09-16 22:08:36 +0200

    add: reexports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
+export { Project } from './w3ads/Project';
 export { Scene } from "./w3ads/Scene";
 export { PhysicsContext, PhysicsColliderFactory } from "./w3ads/PhysicsContext";
 export { GraphicsContext } from "./w3ads/GraphicsContext";
