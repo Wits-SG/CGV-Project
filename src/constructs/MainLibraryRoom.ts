3c512243c653137e57d0e63570a38a0d9caa2be3
Zach SChwark <zachs1708@gmail.com>
2023-09-22 02:50:21 +0200

    add main library construct design

--- src/constructs/MainLibraryRoom.ts
+++ src/constructs/MainLibraryRoom.ts
@@ -3,10 +3,23 @@ import { Construct, GraphicsContext, PhysicsContext, GraphicsPrimitiveFactory, P
 
 export class MainLibraryConstruct extends Construct {
 
+    lightHemisphere!: THREE.HemisphereLight;
+    lightDirectional!: THREE.DirectionalLight;
     walls!: Array<THREE.Mesh>;
     floors!: Array<THREE.Mesh>;
     ceilings!: Array<THREE.Mesh>;
-    bookshelfObject!: THREE.Group;
+    bookShelfs!: Array<THREE.Group>;
+    FbookShelfs!: Array<THREE.Group>;
+    bookshelf!: THREE.Group;
+    floorTexture!: THREE.MeshBasicMaterial;
+    ceilingTexture!: THREE.MeshBasicMaterial;
+    wallsTexture!: THREE.MeshBasicMaterial;
+    textureFloorData!: any;
+    textureCeilingData!: any;
+    textureWallsData!: any;
+    character!:THREE.Mesh;
+    table!: THREE.Group;
+    tables!: Array<THREE.Group>;
 
     constructor(graphics: GraphicsContext, physics: PhysicsContext ) {
         super(graphics, physics);
@@ -14,8 +27,40 @@ export class MainLibraryConstruct extends Construct {
 
     create() {}
 
-    load() {
+    async load(): Promise<void>{
+        try{
+        const gltfData: any = await this.graphics.loadModel('assets/BookShelf/scene.gltf');
+        this.bookshelf = gltfData.scene;
+        }catch(e){
+            console.error(e);
+        }
 
+        try{
+            const gltfData: any = await this.graphics.loadModel('assets/wooden_table/scene.gltf');
+            this.table = gltfData.scene;
+            }catch(e){
+                console.error(e);
+            }
+
+        try{
+            this.textureFloorData = await this.graphics.loadTexture('assets/Poured_Concrete/ConcretePoured001_COL_2K_METALNESS.png');
+        }catch(e){
+            console.error(e);
+        }
+        try{
+            this.textureCeilingData = await this.graphics.loadTexture('assets/colorful-mexican-architecture-urban-landscape.jpg');
+            this.textureCeilingData.wrapS = this.textureCeilingData.wrapT = THREE.RepeatWrapping;
+            this.textureCeilingData.repeat.set(1, 6);
+        }catch(e){
+            console.error(e);
+        }
+        try{
+            this.textureWallsData = await this.graphics.loadTexture('assets/Material.001_baseColor.png');
+            this.textureWallsData.wrapS = this.textureCeilingData.wrapT = THREE.RepeatWrapping;
+            this.textureWallsData.repeat.set(4, 1);
+        }catch(e){
+            console.error(e);
+        }
     }
 
     build() {
@@ -30,11 +75,38 @@ export class MainLibraryConstruct extends Construct {
         const ceilingVertices = [[0,10,0],[60,10,0],[-60,10,0]];
         const ceilingScale = [[80, 225,0],[40, 25,0],[40, 25,0]];
 
+        const bookShelfsVertices = [[-25,-10,100], [-25,-10,90],[-25,-10,80],[-25,-10,70],[-25,-10,60],[-25,-10,50], [-25,-10,40],[-25,-10,30],[-25,-10,20],
+        [-18.5,-10,100], [-18.5,-10,90],[-18.5,-10,80],[-18.5,-10,70],[-18.5,-10,60],[-18.5,-10,70],[-18.5,-10,60],[-18.5,-10,50], [-18.5,-10,40],[-18.5,-10,30],[-18.5,-10,20],
+        [25,-10,100], [25,-10,90],[25,-10,80],[25,-10,70],[25,-10,60], [25,-10,50], [25,-10,40],[25,-10,30],[25,-10,20],
+        [18.5,-10,100], [18.5,-10,90],[18.5,-10,80],[18.5,-10,70],[18.5,-10,60], [18.5,-10,50], [18.5,-10,40],[18.5,-10,30],[18.5,-10,20],
+
+        [-25,-10,-100], [-25,-10,-90],[-25,-10,-80],[-25,-10,-70],[-25,-10,-60],[-25,-10,-50], [-25,-10,-40],[-25,-10,-30],[-25,-10,-20],
+        [-18.5,-10,-100], [-18.5,-10,-90],[-18.5,-10,-80],[-18.5,-10,-70],[-18.5,-10,-60],[-18.5,-10,-70],[-18.5,-10,-60],[-18.5,-10,-50], [-18.5,-10,-40],[-18.5,-10,-30],[-18.5,-10,-20],
+        [25,-10,-100], [25,-10,-90],[25,-10,-80],[25,-10,-70],[25,-10,-60], [25,-10,-50], [25,-10,-40],[25,-10,-30],[25,-10,-20],
+        [18.5,-10,-100], [18.5,-10,-90],[18.5,-10,-80],[18.5,-10,-70],[18.5,-10,-60], [18.5,-10,-50], [18.5,-10,-40],[18.5,-10,-30],[18.5,-10,-20]
+        ];
+
+        const tableVertices = [[-2,111.5,100], [-2,111.5,90],[-2,111.5,80],[-2,111.5,70],[-2,111.5,60],[-2,111.5,50], [-2,111.5,40],[-2,111.5,30],[-2,111.5,20],
+        [2,111.5,100], [2,111.5,90],[2,111.5,80],[2,111.5,70],[2,111.5,60],[2,111.5,50], [2,111.5,40],[2,111.5,30],[2,111.5,20],
+        [-2,111.5,-100], [-2,111.5,-90],[-2,111.5,-80],[-2,111.5,-70],[-2,111.5,-60],[-2,111.5,-50], [-2,111.5,-40],[-2,111.5,-30],[-2,111.5,-20],
+        [2,111.5,-100], [2,111.5,-90],[2,111.5,-80],[2,111.5,-70],[2,111.5,-60],[2,111.5,-50], [2,111.5,-40],[2,111.5,-30],[2,111.5,-20],
+        ];
+
+
+        this.character = GraphicsPrimitiveFactory.box({
+            position: { x: 0, y: -8.5, z:95},
+            scale: { x: 2, y: 3, z: 1},
+            rotation: { x: 0, y: 0, z: 0 },
+            colour: 0x98fb98,
+            shadows: true,
+        });
+        this.graphics.add(this.character);
+
         this.walls = [];
        for(let i =0; i<10; i++){
             const geometry = new THREE.PlaneGeometry( 1, 1);
-            const material = new THREE.MeshBasicMaterial( { color:  0xC0C0C0, side: THREE.DoubleSide } );
-            const wall = new THREE.Mesh(geometry,material);
+            this.wallsTexture = new THREE.MeshBasicMaterial( { map: this.textureWallsData, side: THREE.DoubleSide } );
+            const wall = new THREE.Mesh(geometry, this.wallsTexture);
             wall.scale.set(scaleArr[i][0],scaleArr[i][1],scaleArr[i][2]);
             wall.position.set(vertices[i][0],vertices[i][1],vertices[i][2]);
             wall.rotation.set(rotation[i][0],rotation[i][1],rotation[i][2]);
@@ -45,8 +117,8 @@ export class MainLibraryConstruct extends Construct {
         this.floors = [];
         for(let i = 0; i<3; i++){
             const geometry = new THREE.PlaneGeometry( 1, 1);
-            const material = new THREE.MeshBasicMaterial( { color:  0xB8860B, side: THREE.DoubleSide } );
-            const floor= new THREE.Mesh(geometry,material);
+            this.floorTexture = new THREE.MeshBasicMaterial( { map: this.textureFloorData, side: THREE.DoubleSide } );
+            const floor= new THREE.Mesh(geometry, this.floorTexture);
             floor.position.set(floorVertices[i][0],floorVertices[i][1],floorVertices[i][2]);
             floor.rotation.set(Math.PI/2,0,0);
             floor.scale.set(floorScale[i][0],floorScale[i][1],floorScale[i][2]);
@@ -54,12 +126,44 @@ export class MainLibraryConstruct extends Construct {
             this.physics.addStatic(floor,PhysicsColliderFactory.box(floorScale[i][0]/2, floorScale[i][0]/2, 0.1));
             this.graphics.add( floor);
         }
+        this.bookShelfs = [];
+        for ( let i = 0; i<bookShelfsVertices.length; i++){
+            const tempBookShelf = this.bookshelf.clone();
+            tempBookShelf.position.set(bookShelfsVertices[i][0],bookShelfsVertices[i][1],bookShelfsVertices[i][2]);
+            tempBookShelf.rotation.set(0,Math.PI/2,0);
+            tempBookShelf.scale.set(2.25,2.25,2.25);
+            this.physics.addStatic(tempBookShelf,PhysicsColliderFactory.box(2.25/2, 2.25/2, 1.1));
+            this.graphics.add(tempBookShelf);
+            this.bookShelfs.push(tempBookShelf);
+        }
+
+        this.FbookShelfs = [];
+        for ( let i = 0; i<bookShelfsVertices.length; i++){
+            const tempBookShelf = this.bookshelf.clone();
+            tempBookShelf.position.set(bookShelfsVertices[i][0],bookShelfsVertices[i][1],bookShelfsVertices[i][2]+2);
+            tempBookShelf.rotation.set(0,-Math.PI/2,0);
+            tempBookShelf.scale.set(2.25,2.25,2.25);
+            this.physics.addStatic(tempBookShelf,PhysicsColliderFactory.box(2.25/2, 2.25/2, 1.1));
+            this.graphics.add(tempBookShelf);
+            this.bookShelfs.push(tempBookShelf);
+        }
+
+        this.tables = [];
+        for ( let i = 0; i<tableVertices.length; i++){
+            const tempTable= this.table.clone();
+            tempTable.position.set(tableVertices[i][0],tableVertices[i][1],tableVertices[i][2]+2);
+            tempTable.rotation.set(0,Math.PI,0);
+            tempTable.scale.set(2,1.5,2);
+            this.physics.addStatic(tempTable,PhysicsColliderFactory.box(1, 0.75, 1));
+            this.graphics.add(tempTable);
+            this.tables.push(tempTable);
+        }
 
         this.ceilings = [];
         for(let i = 0; i<3; i++){
             const geometry = new THREE.PlaneGeometry( 1, 1);
-            const material = new THREE.MeshBasicMaterial( { color:  0xfff2cc, side: THREE.DoubleSide } );
-            const ceiling = new THREE.Mesh(geometry,material);
+            this.ceilingTexture= new THREE.MeshBasicMaterial( { map: this.textureCeilingData, side: THREE.DoubleSide } );
+            const ceiling = new THREE.Mesh(geometry,this.ceilingTexture);
             ceiling.position.set(ceilingVertices[i][0],ceilingVertices[i][1],ceilingVertices[i][2]);
             ceiling.rotation.set(Math.PI/2,0,0);
             ceiling.scale.set(ceilingScale[i][0],ceilingScale[i][1],ceilingScale[i][2]);
@@ -67,6 +171,27 @@ export class MainLibraryConstruct extends Construct {
             this.physics.addStatic(ceiling,PhysicsColliderFactory.box(ceilingScale[i][0]/2, ceilingScale[i][0]/2, 0.1));
             this.graphics.add( ceiling);
         }
+
+        this.lightHemisphere = new THREE.HemisphereLight(0xffffff, 0xffffff,1.5);
+        this.lightHemisphere.color.setHSL(0.6, 0.6, 0.6);
+        this.lightHemisphere.groundColor.setHSL(0.1, 1, 0.4);
+        this.lightHemisphere.position.set(0, 100, 0);
+        
+        this.lightDirectional = new THREE.DirectionalLight(0xffffff, 1);
+        this.lightDirectional.color.setHSL(0.1, 1, 0.95);
+        this.lightDirectional.position.set(0, 20, 100);
+        this.lightDirectional.position.multiplyScalar(100);
+        this.lightDirectional.castShadow = true;
+        this.lightDirectional.shadow.mapSize.width = 2048;
+        this.lightDirectional.shadow.mapSize.height = 2048;
+
+        this.lightDirectional.shadow.camera.left = -50;
+        this.lightDirectional.shadow.camera.right = 50;
+        this.lightDirectional.shadow.camera.top = 50;
+        this.lightDirectional.shadow.camera.bottom = -50;
+
+        this.graphics.add(this.lightHemisphere);
+        this.graphics.add(this.lightDirectional);
         
     }
 
--- src/scenes/Sandbox.ts
+++ src/scenes/Sandbox.ts
@@ -15,7 +15,8 @@ export class SandboxScene extends Scene {
     walls!: Array<THREE.Mesh>;
     ballKinematic!: THREE.Mesh;
     //testConstruct: TestConstruct;
-    mainLibraryConstruct: MainLibraryConstruct;
+    k!: THREE.Mesh;
+    mainLibrary!: MainLibraryConstruct;
 
     controls!: OrbitControls;
 
@@ -27,36 +28,39 @@ export class SandboxScene extends Scene {
 
        // this.testConstruct = new TestConstruct(this.graphics, this.physics);
        // this.addConstruct(this.testConstruct);
-        this.mainLibraryConstruct = new MainLibraryConstruct(this.graphics, this.physics);
-        this.addConstruct(this.mainLibraryConstruct);
-
+        this.mainLibrary = new MainLibraryConstruct(this.graphics,this.physics);
+        this.addConstruct(this.mainLibrary);
     }
 
     create(): void {
-        //this.player.create();
+        // this.player.create();
     }
 
-    load(): void {
-        //this.player.load();
+    async load(): Promise<void> {
+        const gltfData: any = await this.graphics.loadModel('assets/officer-k/scene.gltf');
+        this.k = gltfData.scene;
     }
 
     build(): void {
-       // this.player.build();
-       // this.player.setPosition(0, 0, 5);
+        // this.player.build();
+        // this.player.setPosition(0, 0, 5);
         this.graphics.mainCamera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 2000);
-        this.graphics.mainCamera.position.set(20, 100, 20);
-        this.graphics.mainCamera.lookAt(0, 0, 0);
+        this.graphics.mainCamera.position.set(1, 50, 1);
+        this.graphics.mainCamera.lookAt(0, 0,-50);
         this.controls = new OrbitControls(this.graphics.mainCamera, this.graphics.renderer.domElement);
 
-      /* this.floor = GraphicsPrimitiveFactory.box({
+        this.k.position.set(0, -10, 0);
+        this.k.scale.set(1, 1, 1);
+
+       /* this.floor = GraphicsPrimitiveFactory.box({
             position: { x: 0, y: -1, z: 0 },
             scale: { x: 1000, y: 0.1, z: 1000 },
             rotation: { x: 0, y: 0, z: 0 },
             colour: 0x98fb98,
             shadows: true,
-        });
+        });*/
 
-        this.walls = [];
+       /* this.walls = [];
         for (let i = 0; i < 4; ++i) {
             this.walls.push(
                 GraphicsPrimitiveFactory.box({
@@ -115,14 +119,13 @@ export class SandboxScene extends Scene {
             radius: 1,
             colour: 0xff0000,
             shadows: true,
-        })
+        });
         this.graphics.add(this.ballKinematic);
         this.physics.addKinematic(this.ballKinematic, PhysicsColliderFactory.sphere(1), {
             mass: 1,
             linearVelocity: { x: 0, y: 0, z: 1 },
             friction: 0,
-        })*/
-
+        });*/
 
         
         this.lightHemisphere = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.1);
@@ -145,8 +148,9 @@ export class SandboxScene extends Scene {
 
 
         //this.graphics.add(this.floor);
-        this.graphics.add(this.lightHemisphere);
-        this.graphics.add(this.lightDirectional);
+        //this.graphics.add(this.lightHemisphere);
+        //this.graphics.add(this.lightDirectional);
+       //this.graphics.add(this.k);
 
         //this.physics.addStatic(this.floor, PhysicsColliderFactory.box(500, 0.05, 500))
     }
@@ -154,168 +158,6 @@ export class SandboxScene extends Scene {
     update(time: TimeS): void {
         // this.player.update();
         //this.physics.moveKinematic(this.ballKinematic, 0, 0, 0.1 * Math.sin(time));
-    }
-
-    destroy(): void {
-    }
-
-}
-import * as THREE from 'three';
-import { GraphicsPrimitiveFactory, PhysicsColliderFactory, Scene } from '../lib';
-//@ts-expect-error
-import { OrbitControls } from 'three/addons/controls/OrbitControls';
-import { TimeS } from '../lib/w3ads/types/misc.type';
-import { TestConstruct } from '../constructs/TestConstruct';
-
-export class SandboxScene extends Scene {
-
-    lightHemisphere!: THREE.HemisphereLight;
-    lightDirectional!: THREE.DirectionalLight;
-
-    floor!: THREE.Mesh;
-    walls!: Array<THREE.Mesh>;
-    ballKinematic!: THREE.Mesh;
-    testConstruct: TestConstruct;
-    k!: THREE.Mesh;
-
-    controls!: OrbitControls;
-
-    constructor(AmmoLib: any) {
-        super(
-            'Sandbox',
-            AmmoLib
-        );
-
-        this.testConstruct = new TestConstruct(this.graphics, this.physics);
-        this.addConstruct(this.testConstruct);
-
-    }
-
-    create(): void {
-        // this.player.create();
-    }
-
-    async load(): Promise<void> {
-        const gltfData: any = await this.graphics.loadModel('assets/officer-k/scene.gltf');
-        this.k = gltfData.scene;
-    }
-
-    build(): void {
-        // this.player.build();
-        // this.player.setPosition(0, 0, 5);
-        this.graphics.mainCamera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 2000);
-        this.graphics.mainCamera.position.set(20, 100, 20);
-        this.graphics.mainCamera.lookAt(0, 0, 0);
-        this.controls = new OrbitControls(this.graphics.mainCamera, this.graphics.renderer.domElement);
-
-        this.k.position.set(0, 0, 0);
-
-        this.floor = GraphicsPrimitiveFactory.box({
-            position: { x: 0, y: -1, z: 0 },
-            scale: { x: 1000, y: 0.1, z: 1000 },
-            rotation: { x: 0, y: 0, z: 0 },
-            colour: 0x98fb98,
-            shadows: true,
-        });
-
-        this.walls = [];
-        for (let i = 0; i < 4; ++i) {
-            this.walls.push(
-                GraphicsPrimitiveFactory.box({
-                    position: { x: 0, y: 0, z: i * 10 + 10 },
-                    scale: { x: 40, y: 2, z: 0.2 },
-                    rotation: { x: 0, y: 0, z: 0 },
-                    colour: 0x0000ff,
-                    shadows: true,
-                })
-            );
-
-            this.physics.addStatic(this.walls[i], PhysicsColliderFactory.box(20, 1, 0.1));
-            this.graphics.add(this.walls[i]);
-        }
-        for (let i = 4; i < 8; ++i) {
-            this.walls.push(
-                GraphicsPrimitiveFactory.box({
-                    position: { x: 0, y: 0, z: (i - 4) * -10 - 10 },
-                    scale: { x: 40, y: 2, z: 0.2 },
-                    rotation: { x: 0, y: 0, z: 0 },
-                    colour: 0x0000ff,
-                    shadows: true,
-                })
-            );
-
-            this.physics.addStatic(this.walls[i], PhysicsColliderFactory.box(20, 1, 0.1));
-            this.graphics.add(this.walls[i]);
-        }
-        this.walls.push(
-            GraphicsPrimitiveFactory.box({
-                position: { x: 25, y: 0, z: 0 },
-                scale: { x: 0.2, y: 2, z: 80 },
-                rotation: { x: 0, y: 0, z: 0 },
-                colour: 0x0000ff,
-                shadows: true,
-            })
-        );
-        this.physics.addStatic(this.walls[8], PhysicsColliderFactory.box(0.1, 1, 40));
-        this.graphics.add(this.walls[8]);
-
-        this.walls.push(
-            GraphicsPrimitiveFactory.box({
-                position: { x: -25, y: 0, z: 0 },
-                scale: { x: 0.2, y: 2, z: 80 },
-                rotation: { x: 0, y: 0, z: 0 },
-                colour: 0x0000ff,
-                shadows: true,
-            })
-        );
-        this.physics.addStatic(this.walls[9], PhysicsColliderFactory.box(0.1, 1, 40));
-        this.graphics.add(this.walls[9]);
-
-        this.ballKinematic = GraphicsPrimitiveFactory.sphere({
-            position: { x: 0, y: 0, z: 0 },
-            rotation: { x: 0, y: 0, z: 0 },
-            radius: 1,
-            colour: 0xff0000,
-            shadows: true,
-        })
-        this.graphics.add(this.ballKinematic);
-        this.physics.addKinematic(this.ballKinematic, PhysicsColliderFactory.sphere(1), {
-            mass: 1,
-            linearVelocity: { x: 0, y: 0, z: 1 },
-            friction: 0,
-        })
-
-        
-        this.lightHemisphere = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.1);
-        this.lightHemisphere.color.setHSL(0.6, 0.6, 0.6);
-        this.lightHemisphere.groundColor.setHSL(0.1, 1, 0.4);
-        this.lightHemisphere.position.set(0, 50, 0);
-        
-        this.lightDirectional = new THREE.DirectionalLight(0xffffff, 1);
-        this.lightDirectional.color.setHSL(0.1, 1, 0.95);
-        this.lightDirectional.position.set(-1, 1.75, 1);
-        this.lightDirectional.position.multiplyScalar(100);
-        this.lightDirectional.castShadow = true;
-        this.lightDirectional.shadow.mapSize.width = 2048;
-        this.lightDirectional.shadow.mapSize.height = 2048;
-
-        this.lightDirectional.shadow.camera.left = -50;
-        this.lightDirectional.shadow.camera.right = 50;
-        this.lightDirectional.shadow.camera.top = 50;
-        this.lightDirectional.shadow.camera.bottom = -50;
-
-
-        this.graphics.add(this.floor);
-        this.graphics.add(this.lightHemisphere);
-        this.graphics.add(this.lightDirectional);
-        this.graphics.add(this.k);
-
-        this.physics.addStatic(this.floor, PhysicsColliderFactory.box(500, 0.05, 500))
-    }
-
-    update(time: TimeS): void {
-        // this.player.update();
-        this.physics.moveKinematic(this.ballKinematic, 0, 0, 0.1 * Math.sin(time));
     }
 
     destroy(): void {
