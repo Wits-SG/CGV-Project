81c40a2bcd7dd6a6055c1f1f23083b1c63d21787
Orwellian <orwellian225@gmail.com>
2023-10-04 23:03:50 +0200

    add: Picking up and placing objects

--- src/lib/w3ads/InteractManager.ts
+++ src/lib/w3ads/InteractManager.ts
@@ -4,7 +4,8 @@ import * as THREE from 'three';
 type InteractableObject = {
     object: THREE.Object3D,
     radius: number,
-    onInteract: Function
+    onInteract: Function,
+    canPickup: boolean
 }
 
 // An object that can interact / pick up other objects
@@ -13,42 +14,45 @@ type InteractingObject = {
     onPickup: Function,
 }
 
-// An object that can be picked up and placed down
-type PickupObject = {
-    object: THREE.Mesh,
-    onPickup: Function,
-}
-
 // An object that tracks possible places to place pickupable objects
 type PickupSpot = {
     spot: THREE.Object3D,
-    currentObject: THREE.Object3D,
-    radius: number
+    radius: number,
+    onPlace: Function
 }
 
 export class InteractManager {
     public interactingObjects: Array<InteractingObject>;
     public interactableObjects: Array<InteractableObject>;
-    public pickupObjects: Array<PickupObject>;
     public pickupSpots: Array<PickupSpot>;
 
     constructor() {
         this.interactableObjects = [];
         this.interactingObjects = [];
-        this.pickupObjects = [];
         this.pickupSpots = [];
     }
 
     addInteracting(object: THREE.Object3D, onPickup: Function) {
         this.interactingObjects.push({
-            object, onPickup
+            object, onPickup, 
         } satisfies InteractingObject);
     }
 
-    addInteractable(object: THREE.Object3D, radius: number, onInteract: Function) {
+    addInteractable(object: THREE.Object3D, radius: number, onInteract: Function, canPickup: boolean | undefined = undefined) {
         this.interactableObjects.push({
-            object, radius, onInteract
+            object, radius, onInteract, canPickup: canPickup != undefined ? canPickup : false
         } satisfies InteractableObject);
+    }
+
+    addPickupObject(object: THREE.Mesh, radius: number, inHandScale: number, onPickup: Function) {
+        object.userData.inHandScale = inHandScale;
+        this.addInteractable(object, radius, onPickup, true);
+    }
+
+    addPickupSpot(object: THREE.Object3D, radius: number, onPlace: Function) {
+        this.pickupSpots.push({
+            spot: object, radius, onPlace
+        } satisfies PickupSpot)
     }
 
     update() {
@@ -76,15 +80,52 @@ export class InteractManager {
                     minDistanceSquared = distanceSquared;
                 }
             }
+
             // i.e. if no in range object was found
-            if (minDistanceSquared <= this.interactableObjects[minObj].radius**2 ) {
-                interacting.object.userData.canInteract = true;
-                interacting.object.userData.onInteract = this.interactableObjects[minObj].onInteract;
+            if (minDistanceSquared <= this.interactableObjects[minObj].radius**2) {
+                if (!this.interactableObjects[minObj].canPickup) {
+                    interacting.object.userData.canInteract = true;
+                    interacting.object.userData.onInteract = this.interactableObjects[minObj].onInteract;
+                } else {
+                    interacting.object.userData.canInteract = true;
+                    interacting.object.userData.onInteract = () => {
+                        this.interactableObjects[minObj].onInteract();
+                        interacting.onPickup(this.interactableObjects[minObj].object);
+                    }
+                }
             } else {
                 interacting.object.userData.canInteract = false;
             }
 
-            // Pickup search to find closest object
+            // Place object search
+            let minSpot = 0;
+            let minSpotDistanceSquared = Number.MAX_VALUE;
+            for (let i = 0; i < this.pickupSpots.length; ++i) {
+                const vals = {
+                    x2: (
+                        interacting.object.position.x - this.pickupSpots[i].spot.position.x
+                    )**2,
+                    y2: (
+                        interacting.object.position.y - this.pickupSpots[i].spot.position.y
+                    )**2,
+                    z2: (
+                        interacting.object.position.z - this.pickupSpots[i].spot.position.z
+                    )**2,
+                };
+                const distanceSquared = vals.x2 + vals.y2 + vals.z2;
+
+                if (distanceSquared < minSpotDistanceSquared) {
+                    minSpot = i;
+                    minSpotDistanceSquared = distanceSquared;
+                }
+            }
+
+            if (minSpotDistanceSquared <= this.pickupSpots[minSpot].radius**2) {
+                interacting.object.userData.canPlace = true;
+                interacting.object.userData.onPlace = this.pickupSpots[minSpot].onPlace;
+            } else {
+                interacting.object.userData.canPlace = false;
+            }
 
         }
     }
