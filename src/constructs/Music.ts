af2a9a2d3840fcd98876107b9fdac988f3fc6883
Ashlea <smithashlea@gmail.com>
2023-10-07 13:36:21 +0200

    Add: all instruments onto the scene

--- src/constructs/Music.ts
+++ src/constructs/Music.ts
@@ -36,26 +36,26 @@ export class MusicConstruct extends Construct{
             console.error(e);
         }
 
-        // try {//guitar object
-        //     const gltfData: any = await this.graphics.loadModel('assets/music/acoustic_guitar/scene.gltf');
-        //     this.guitar = gltfData.scene;
-        // } catch (e: any) {
-        //     console.error(e);
-        // }
+        try {//guitar object
+            const gltfData: any = await this.graphics.loadModel('assets/music/acoustic_guitar/scene.gltf');
+            this.guitar = gltfData.scene;
+        } catch (e: any) {
+            console.error(e);
+        }
 
-        // try {//piano object
-        //     const gltfData: any = await this.graphics.loadModel('assets/music/piano_low_poly/scene.gltf');
-        //     this.piano = gltfData.scene;
-        // } catch (e: any) {
-        //     console.error(e);
-        // }
+        try {//piano object
+            const gltfData: any = await this.graphics.loadModel('assets/music/piano_low_poly/scene.gltf');
+            this.piano = gltfData.scene;
+        } catch (e: any) {
+            console.error(e);
+        }
 
-        // try {//gramophone object
-        //     const gltfData: any = await this.graphics.loadModel('assets/music/gramophone/scene.gltf');
-        //     this.gram = gltfData.scene;
-        // } catch (e: any) {
-        //     console.error(e);
-        // }
+        try {//gramophone object
+            const gltfData: any = await this.graphics.loadModel('assets/music/gramophone/scene.gltf');
+            this.gram = gltfData.scene;
+        } catch (e: any) {
+            console.error(e);
+        }
 
         try {//gramophone object
             const gltfData: any = await this.graphics.loadModel('assets/music/manhasset_music_stand/scene.gltf');
@@ -85,21 +85,19 @@ export class MusicConstruct extends Construct{
         this.physics.addStatic(this.floor, PhysicsColliderFactory.box(30, 0.5, 30)); 
 
 
-        //phone
-        // const tempPhone = this.phone.clone();
-        // tempPhone.position.set(0, 10, -2);
-        // tempPhone.rotation.set(-Math.PI/2, Math.PI/4, 0);
-        // tempPhone.scale.set(1,1,1);
-        // 
-        // this.floor.add(tempPhone);
+        
 
         const guitarGeom = new THREE.BoxGeometry(3, 2, 3); 
         const guitarBoxMat = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
         const guitarBoxMatBlue = new THREE.MeshLambertMaterial({ color: 0x0000ff });
         const guitarBox = new THREE.Mesh(guitarGeom, guitarBoxMat);
-        guitarBox.position.set(0, 2, 25);
-        guitarBox.rotation.set(0, Math.PI/4, 0);
-        this.floor.add(guitarBox);
+        guitarBox.position.set(-10, 1, 4);
+        //guitarBox.rotation.set(0, Math.PI/4, 0);
+        this.guitar.position.set(-10,1,4);
+        this.guitar.scale.set(0.05,0.05,0.05);
+        this.guitar.rotation.set(0,3*Math.PI/4,0);
+        this.floor.add(this.guitar);
+        //this.floor.add(guitarBox);
         this.physics.addStatic(guitarBox, PhysicsColliderFactory.box(1.5, 1, 1.5));
 
         this.interactions.addInteractable(guitarBox, 5, () => {
@@ -114,25 +112,50 @@ export class MusicConstruct extends Construct{
             // Check if order array size = number of instruments  --> done first for performance optimisation
             // then check if instruments are in correct order --> done second for performance optimisation
             // then spawn crystal
-        })
+        });
 
-        // this.conductorStand.position.set(0, 2, 25);
-        // this.floor.add(this.conductorStand);
-        // phoneBox.removeFromParent();
-        // this.add(phoneBox);
+        //PIANO
+        const pianoGeom = new THREE.BoxGeometry(3, 2, 3); 
+        const pianoBoxMat = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+        const pianoBoxMatBlue = new THREE.MeshLambertMaterial({ color: 0x0000ff });
+        const pianoBox = new THREE.Mesh(pianoGeom, pianoBoxMat);
+        pianoBox.position.set(0, 1, 4);
 
-        //Add guitar
-        // const tempGuitar = this.guitar.clone();
-        // tempGuitar.position.set(-20.5, -10.5, -0.6);
-        // tempGuitar.rotation.set(-Math.PI/2, Math.PI/4, 0);
-        // tempGuitar.scale.set(0.2,0.2,0.2);
-        // this.floor.add(this.guitar);
+        this.piano.position.set(0,1,4);
+        this.piano.scale.set(0.5,0.5,0.5);
+        this.piano.rotation.set(0, -Math.PI/2,0);
+        this.floor.add(this.piano);
+        //this.floor.add(pianoBox);
+        this.physics.addStatic(pianoBox, PhysicsColliderFactory.box(1.5, 1, 1.5));
 
-        // const tempPiano = this.piano.clone();
-        // tempPiano.position.set(-1.5, -15.5, -0.6);
-        // tempPiano.rotation.set(-Math.PI/2, Math.PI/2, 0);
-        // tempPiano.scale.set(1.5,1.5,1.5);
-        // this.floor.add(tempPiano);
+
+        //GRAMOPHONE
+        const gramGeom = new THREE.BoxGeometry(3, 2, 3); 
+        const gramBoxMat = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+        const gramBoxMatBlue = new THREE.MeshLambertMaterial({ color: 0x0000ff });
+        const gramBox = new THREE.Mesh(gramGeom, gramBoxMat);
+        gramBox.position.set(10, 1, 4);
+        this.gram.position.set(10,1,4);
+        this.gram.scale.set(5,5,5);
+        this.gram.rotation.set(0,3*Math.PI/4,0);
+        //this.floor.add(gramBox);
+        this.floor.add(this.gram);
+
+        this.physics.addStatic(gramBox, PhysicsColliderFactory.box(1.5, 1, 1.5));
+        
+
+       //STAND
+       const standGeom = new THREE.BoxGeometry(2, 2, 2); 
+       const standBoxMat = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+       const standBoxMatBlue = new THREE.MeshLambertMaterial({ color: 0x0000ff });
+       const standBox = new THREE.Mesh(standGeom, standBoxMat);
+       standBox.position.set(0, 1, -8);
+       this.conductorStand.position.set(0 , 1 ,-8);
+       this.conductorStand.scale.set(0.1,0.1,0.1)
+       this.floor.add(this.conductorStand);
+       //this.floor.add(standBox)
+       
+       this.physics.addStatic(standBox, PhysicsColliderFactory.box(1, 1, 1));
 
         // const tempGram = this.gram.clone();
         // tempGram.position.set(20, -15, 0);
@@ -146,26 +169,26 @@ export class MusicConstruct extends Construct{
         // tempStand.scale.set(0.5,0.5,0.5);
         // this.floor.add(tempStand);
 
-        // // Add point lights at the corners of board
-        // const cornerLight1 = new THREE.PointLight(0xffffff, 500, 100);
-        // cornerLight1.position.set(-20, -20, -20); // Adjust the position as per your needs
-        // this.floor.add(cornerLight1);
+        // Add point lights at the corners of board
+        const cornerLight1 = new THREE.PointLight(0xffffff, 500, 100);
+        cornerLight1.position.set(-20, -20, -20); // Adjust the position as per your needs
+        this.floor.add(cornerLight1);
 
-        // const cornerLight2 = new THREE.PointLight(0xffffff, 500, 100);
-        // cornerLight2.position.set(20, -10, -10); 
-        // this.floor.add(cornerLight2);
+        const cornerLight2 = new THREE.PointLight(0xffffff, 500, 100);
+        cornerLight2.position.set(20, -10, -10); 
+        this.floor.add(cornerLight2);
 
-        // const cornerLight3 = new THREE.PointLight(0xffffff, 500, 100);
-        // cornerLight3.position.set(-15, 15, -10); 
-        // this.floor.add(cornerLight3);
+        const cornerLight3 = new THREE.PointLight(0xffffff, 500, 100);
+        cornerLight3.position.set(-15, 15, -10); 
+        this.floor.add(cornerLight3);
 
-        // const cornerLight4 = new THREE.PointLight(0xffffff, 500, 100);
-        // cornerLight4.position.set(15, 15, -10); 
-        // this.floor.add(cornerLight4);
+        const cornerLight4 = new THREE.PointLight(0xffffff, 500, 100);
+        cornerLight4.position.set(15, 15, -10); 
+        this.floor.add(cornerLight4);
 
-        // const middleLight = new THREE.PointLight(0xffffff, 500, 100);
-        // middleLight.position.set(0,0,-10);
-        // this.floor.add(middleLight);
+        const middleLight = new THREE.PointLight(0xffffff, 500, 100);
+        middleLight.position.set(0,0,-10);
+        this.floor.add(middleLight);
 
 
 
