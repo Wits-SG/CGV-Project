95f93b6c2093130d02a7574c4b4f8bdbfb2abb8b
Orwellian <orwellian225@gmail.com>
2023-10-09 19:49:38 +0200

    add: Level 1

---
+++ src/scenes/Level1.ts
@@ -0,0 +1,32 @@
+import { MainLibraryConstruct } from "../constructs/MainLibraryRoom";
+import { Player } from "../constructs/Player";
+import { Scene } from "../lib";
+
+export class LevelOne extends Scene {
+    player: Player;
+    library: MainLibraryConstruct;
+
+    constructor(AmmoLib: any) {
+        super(
+            'MainMenu',
+            AmmoLib
+        );
+
+        this.player = new Player(this.graphics, this.physics, this.interactions, this.userInterface, 'level1');
+        this.addConstruct(this.player);
+
+        this.library = new MainLibraryConstruct(this.graphics, this.physics, this.interactions, this.userInterface, 1, this.player);
+        this.addConstruct(this.library);
+    }
+
+    create(): void {}
+
+    async load(): Promise<void> {}
+
+    build(): void {}
+
+    update(): void {}
+
+    destroy(): void {}
+
+}
