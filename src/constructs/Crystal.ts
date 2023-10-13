120d64fc2a5282b099cc76c47e940886e3719e3a
Orwellian <orwellian225@gmail.com>
2023-10-13 19:30:12 +0200

    remove: Crystal unused bloom stuff

--- src/constructs/Crystal.ts
+++ src/constructs/Crystal.ts
@@ -1,13 +1,5 @@
 import * as THREE from 'three';
 import { Construct } from '../lib/index';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer';
-import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass';
-import { ShaderPass } from 'three/addons/postprocessing/ShaderPass';
-
-const bloomVertexShader = 'varying vec2 vUv; void main() { vUv = uv; gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 ); }';
-const bloomFragmentShader = 'uniform sampler2D baseTexture; uniform sampler2D bloomTexture; varying vec2 vUv; void main() { gl_FragColor = ( texture2D( baseTexture, vUv ) + vec4(1.0) * texture2D( bloomTexture, vUv ) ); }';
-
-const BLOOM = 2;
 
 export class Crystal extends Construct {
     crystalBody!: THREE.Mesh;
