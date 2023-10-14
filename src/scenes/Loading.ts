cf861732eeaa5149633106decfad75cd89eaa2aa
Orwellian <orwellian225@gmail.com>
2023-10-14 12:33:21 +0200

    add: Loading Spinner

--- src/scenes/Loading.ts
+++ src/scenes/Loading.ts
@@ -8,16 +8,12 @@ import { Crystal } from '../constructs/Crystal';
 export class LoadingScreen extends Scene {
 
     camera!: THREE.PerspectiveCamera;
-
-    container!: HTMLDivElement;
-    root!: HTMLDivElement;
-    menu!: HTMLDivElement;
-    credits!: HTMLDivElement;
-    levelMenus!: Array<HTMLDivElement>;
-
+    loading!: number;
     numCrystals: number;
     door: CrystalDoor;
     crystals: Array<Crystal>;
+
+
 
     constructor(AmmoLib: any) {
         super(
@@ -41,15 +37,18 @@ export class LoadingScreen extends Scene {
             );
             this.addConstruct(this.crystals[i]);
         }
-
-        this.levelMenus = [];
     }
 
-    create(): void {}
+    create(): void {
+
+        this.loading = this.userInterface.addPrompt(`<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_P7sC{transform-origin:center;animation:spinner_svv2 .75s infinite linear}@keyframes spinner_svv2{100%{transform:rotate(360deg)}}</style><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_P7sC"/></svg>`)
+
+    }
 
     async load(): Promise<void> {}
 
     build(): void {
+        this.userInterface.showPrompt(this.loading);
         this.camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 2000);
         this.graphics.mainCamera = this.camera;
         this.graphics.mainCamera.position.set(5, 7, 5);
