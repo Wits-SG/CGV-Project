db3dbb15947e373e1dddd45ad7b122b7c6778ca5
Orwellian <orwellian225@gmail.com>
2023-10-15 20:12:00 +0200

    add: Lighting to office

--- src/constructs/OfficeConstruct.ts
+++ src/constructs/OfficeConstruct.ts
@@ -59,7 +59,6 @@ export class OfficeConstruct extends Construct {
 
         this.fence = new Fence(this.graphics, this.physics, this.interactions, this.userInterface);
         this.addConstruct(this.fence);
-
     }
 
     create(): void {
@@ -227,7 +226,7 @@ export class OfficeConstruct extends Construct {
                     (i === 1 && placedObject === this.wizardHat) ||
                     (i === 2 && placedObject === this.cauldron)) {
                     // if correct, change colour to green
-                    (box.material as THREE.MeshBasicMaterial).color.set(0x00ff00);
+                    (box.material as THREE.MeshLambertMaterial).color.set(0x007c00);
                     // placed object is made uniteracctable
                     placedObject.userData.canInteract = false;
                     this.interactions.interactableObjects = this.interactions.interactableObjects.filter(obj => obj.object !== placedObject);
@@ -264,24 +263,24 @@ export class OfficeConstruct extends Construct {
 
         // floor
 
-        this.floor = new THREE.Mesh(new THREE.BoxGeometry(30, 0.2, 50), new THREE.MeshBasicMaterial({ map: this.floorTexture }));
+        this.floor = new THREE.Mesh(new THREE.BoxGeometry(30, 0.2, 50), new THREE.MeshLambertMaterial({ map: this.floorTexture }));
         this.floor.position.set(0,-0.1,0);
         this.add(this.floor);
         this.physics.addStatic(this.floor,PhysicsColliderFactory.box(15, 0.1 , 25));
 
         // walls
 
-        const wallL = new THREE.Mesh(new THREE.BoxGeometry(0.1, 13.2, 50), new THREE.MeshBasicMaterial({ map: this.wallTexture }));
+        const wallL = new THREE.Mesh(new THREE.BoxGeometry(0.1, 13.2, 50), new THREE.MeshLambertMaterial({ map: this.wallTexture }));
         wallL.position.set(-15,6.6,0);
         this.add(wallL);
         this.physics.addStatic(wallL,PhysicsColliderFactory.box(0.5, 6.6 , 25));
 
-        const wallR = new THREE.Mesh(new THREE.BoxGeometry(0.1, 13.2, 50), new THREE.MeshBasicMaterial({ map: this.wallTexture }));
+        const wallR = new THREE.Mesh(new THREE.BoxGeometry(0.1, 13.2, 50), new THREE.MeshLambertMaterial({ map: this.wallTexture }));
         wallR.position.set(15,6.6,0);
         this.add(wallR);
         this.physics.addStatic(wallR,PhysicsColliderFactory.box(0.5, 6.6 , 25));
 
-        const wallB = new THREE.Mesh(new THREE.BoxGeometry(30, 13.2, 0.1), new THREE.MeshBasicMaterial({ map: this.wallTexture }));
+        const wallB = new THREE.Mesh(new THREE.BoxGeometry(30, 13.2, 0.1), new THREE.MeshLambertMaterial({ map: this.wallTexture }));
         wallB.position.set(0,6.6,-24.95);
         this.add(wallB);
         this.physics.addStatic(wallB,PhysicsColliderFactory.box(15, 6.6 , 0.5));
@@ -293,33 +292,33 @@ export class OfficeConstruct extends Construct {
 
         // small room walls 
 
-        const wallLf = new THREE.Mesh(new THREE.BoxGeometry(10, 13.2, 0.1), new THREE.MeshBasicMaterial({ map: this.wallTexture }));
+        const wallLf = new THREE.Mesh(new THREE.BoxGeometry(10, 13.2, 0.1), new THREE.MeshLambertMaterial({ map: this.wallTexture }));
         wallLf.position.set(10,6.6,-14.7);
         this.add(wallLf);
         this.physics.addStatic(wallLf,PhysicsColliderFactory.box(6, 6.6 , 0.5));
 
-        const wallRf = new THREE.Mesh(new THREE.BoxGeometry(10, 13.2, 0.1), new THREE.MeshBasicMaterial({ map: this.wallTexture }));
+        const wallRf = new THREE.Mesh(new THREE.BoxGeometry(10, 13.2, 0.1), new THREE.MeshLambertMaterial({ map: this.wallTexture }));
         wallRf.position.set(-10,6.6,-14.7);
         this.add(wallRf);
         this.physics.addStatic(wallRf,PhysicsColliderFactory.box(6, 6.6 , 0.5));
 
         // small room roof
 
-        const smallRoof = new THREE.Mesh(new THREE.BoxGeometry(30, 0.5, 10.5), new THREE.MeshBasicMaterial({ map: this.smallRoofTexture }));
+        const smallRoof = new THREE.Mesh(new THREE.BoxGeometry(30, 0.5, 10.5), new THREE.MeshLambertMaterial({ map: this.smallRoofTexture }));
         smallRoof.position.set(0,5.24,-20);
         this.add(smallRoof);
         this.physics.addStatic(smallRoof,PhysicsColliderFactory.box(15, 0.25 , 5.25));
 
         // large room roof
 
-        const largeRoof = new THREE.Mesh(new THREE.BoxGeometry(30, 0.5, 50), new THREE.MeshBasicMaterial({ map: this.roofTexture }));
+        const largeRoof = new THREE.Mesh(new THREE.BoxGeometry(30, 0.5, 50), new THREE.MeshLambertMaterial({ map: this.roofTexture }));
         largeRoof.position.set(0,13.45,0);
         this.add(largeRoof);
         this.physics.addStatic(largeRoof,PhysicsColliderFactory.box(15, 0.25 , 25));
 
         // upper room floor
 
-        const upperFloor = new THREE.Mesh(new THREE.BoxGeometry(30, 0.1, 10.5), new THREE.MeshBasicMaterial({ map: this.floorTexture }));
+        const upperFloor = new THREE.Mesh(new THREE.BoxGeometry(30, 0.1, 10.5), new THREE.MeshLambertMaterial({ map: this.floorTexture }));
         upperFloor.position.set(0,5.47,-20);
         this.add(upperFloor);
 
@@ -327,23 +326,42 @@ export class OfficeConstruct extends Construct {
 
         const rightBlock = new THREE.Mesh(new THREE.BoxGeometry(2.5, 13.2, 6) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
         rightBlock.position.set(14.1, 6.6, -11.5);
-        //this.graphics.add(rightBlock);
+        this.add(rightBlock);
         this.physics.addStatic(rightBlock,PhysicsColliderFactory.box(2, 6.6 , 3.5));
+        rightBlock.removeFromParent();
 
         const leftBlock = new THREE.Mesh(new THREE.BoxGeometry(2.5, 13.2, 6) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
         leftBlock.position.set(-14.1, 6.6, -11.5);
-        //this.graphics.add(leftBlock);
+        this.add(leftBlock);
         this.physics.addStatic(leftBlock,PhysicsColliderFactory.box(2, 6.6 , 3.5));
+        leftBlock.removeFromParent();
 
         const pillarLeftBlock = new THREE.Mesh(new THREE.BoxGeometry(1, 5.8, 3) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
         pillarLeftBlock.position.set(-7, 2.9 , -11);
-        // this.graphics.add(pillarLeftBlock);
+        this.add(pillarLeftBlock);
         this.physics.addStatic(pillarLeftBlock,PhysicsColliderFactory.box(0.6, 2.9 , 1.7));
+        pillarLeftBlock.removeFromParent();
 
         const rightPillarBlock = new THREE.Mesh(new THREE.BoxGeometry(1, 5.8, 3) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
         rightPillarBlock.position.set(7, 2.9 , -11);
-        // this.graphics.add(rightPillarBlock);
+        this.add(rightPillarBlock);
         this.physics.addStatic(rightPillarBlock,PhysicsColliderFactory.box(0.6, 2.9 , 1.7));
+        rightPillarBlock.removeFromParent();
+
+
+        // Lighting
+
+        const roomLight = new THREE.PointLight(0xfae5ac, 200, 0, 1.1);
+        roomLight.position.set(0, 10, 0);
+        this.add(roomLight);
+
+        const backroomLight = new THREE.PointLight(0xfae5ac, 70, 0, 1.1);
+        backroomLight.position.set(0, 0, -19);
+        this.add(backroomLight);
+
+        const upstairsLight = new THREE.PointLight(0xfae5ac, 90, 0, 1.1);
+        upstairsLight.position.set(0, 6, -19);
+        this.add(upstairsLight);
 
         // **** BUILD MODELS **** //
 
@@ -364,13 +382,15 @@ export class OfficeConstruct extends Construct {
 
         const bookShelfOfficePhysics = new THREE.Mesh(new THREE.BoxGeometry(1.35, 4.6, 3.4) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
         bookShelfOfficePhysics.position.set(14.1, 2.3, 20);
-        //this.graphics.add(bookShelfOfficePhysics);
+        this.add(bookShelfOfficePhysics);
         this.physics.addStatic(bookShelfOfficePhysics,PhysicsColliderFactory.box(0.675, 2.3 , 1.7));
+        bookShelfOfficePhysics.removeFromParent();
 
         const secondBookShelfOfficePhysics = new THREE.Mesh(new THREE.BoxGeometry(1.35, 4.6, 3.4) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
         secondBookShelfOfficePhysics.position.set(-14.1, 2.3, -19);
-        //this.graphics.add(secondBookShelfOfficePhysics);
+        this.add(secondBookShelfOfficePhysics);
         this.physics.addStatic(secondBookShelfOfficePhysics,PhysicsColliderFactory.box(0.675, 2.3 , 1.7));
+        secondBookShelfOfficePhysics.removeFromParent();
 
         // alchemyShelf
 
@@ -387,13 +407,15 @@ export class OfficeConstruct extends Construct {
 
         const alchemyShelfPhysics = new THREE.Mesh(new THREE.BoxGeometry(1.65, 2.7, 2.7) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
         alchemyShelfPhysics.position.set(14.3, 3.2, 7);
-        //this.graphics.add(alchemyShelfPhysics);
+        this.add(alchemyShelfPhysics);
         this.physics.addStatic(alchemyShelfPhysics,PhysicsColliderFactory.box(0.825, 1.35 , 1.35));
+        alchemyShelfPhysics.removeFromParent();
 
         const alchemyShelf2Physics = new THREE.Mesh(new THREE.BoxGeometry(1.65, 2.7, 2.7) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
         alchemyShelf2Physics.position.set(14.3, 3.2, -20);
-        //this.graphics.add(alchemyShelf2Physics);
+        this.add(alchemyShelf2Physics);
         this.physics.addStatic(alchemyShelf2Physics,PhysicsColliderFactory.box(0.825, 1.35 , 1.35));
+        alchemyShelf2Physics.removeFromParent();
 
         // firePlace
 
@@ -405,8 +427,9 @@ export class OfficeConstruct extends Construct {
 
         const firePlacePhysics = new THREE.Mesh(new THREE.BoxGeometry(2.4, 3, 1) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
         firePlacePhysics.position.set(0, 1.5, -24.4);
-        // this.graphics.add(firePlacePhysics);
+        this.add(firePlacePhysics);
         this.physics.addStatic(firePlacePhysics,PhysicsColliderFactory.box(1.2, 1.5 , 0.5));
+        firePlacePhysics.removeFromParent();
 
         // table
 
@@ -423,18 +446,20 @@ export class OfficeConstruct extends Construct {
 
          const tablePhysics = new THREE.Mesh(new THREE.BoxGeometry(4.2, 1.55, 2.2) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
          tablePhysics.position.set(0, 0.82, 5);
-        //  this.graphics.add(tablePhysics);
+         this.add(tablePhysics);
          this.physics.addStatic(tablePhysics,PhysicsColliderFactory.box(2.4, 1.1 , 1.3));
+         tablePhysics.removeFromParent();
 
 
          const upstairsTablePhysics = new THREE.Mesh(new THREE.BoxGeometry(4.2, 1.55, 2.2) , new THREE.MeshBasicMaterial({ color: "#0000FF" }));
          upstairsTablePhysics.position.set(0, 6.25, -20);
-        //  this.graphics.add(upstairsTablePhysics);
+         this.add(upstairsTablePhysics);
          this.physics.addStatic(upstairsTablePhysics,PhysicsColliderFactory.box(2.4, 1.1 , 1.3));
+         upstairsTablePhysics.removeFromParent();
 
         // carpet
 
-        this.carpet = new THREE.Mesh(new THREE.BoxGeometry(7, 0.01, 5), new THREE.MeshBasicMaterial({ map: this.carpetTexture }));
+        this.carpet = new THREE.Mesh(new THREE.BoxGeometry(7, 0.01, 5), new THREE.MeshLambertMaterial({ map: this.carpetTexture }));
         this.carpet.position.set(0,0,5);
         this.add(this.carpet);
         this.physics.addStatic(this.floor,PhysicsColliderFactory.box(15, 0.1 , 25));
@@ -472,7 +497,7 @@ export class OfficeConstruct extends Construct {
             if( i != 3){ 
             const box = new THREE.Mesh(
                 new THREE.BoxGeometry(2, 0.2, 2),
-                new THREE.MeshBasicMaterial({ color: 0xff0000 }) // initial color: red
+                new THREE.MeshLambertMaterial({ color: 0x7c0000 }) // initial color: red
             );
             box.position.set(-3 +(i * 3), 0.1, 14);
             this.add(box);
@@ -481,25 +506,26 @@ export class OfficeConstruct extends Construct {
             this.interactions.addPickupSpot(box, 3, (placedObject: THREE.Object3D) => {
                 box.add(placedObject);
                 placedObject.position.set(0,0.5,0);
+
                 if ((placedObject === this.feather)){
                     placedObject.position.set(0,0.4,-0.7);
                     placedObject.scale.set(0.02,0.02,0.02); 
-                    }else if(placedObject == this.cauldron) {
+                } else if (placedObject == this.cauldron) {
                     placedObject.position.set(0,0.5,0);
                     placedObject.scale.set(0.04,0.04,0.04); 
-                    }else{
+                } else {
                     placedObject.position.set(0,1,3.4);
                     placedObject.scale.set(2.5,2.5,2.5) ;
-                    }
+                }
                     
                 this.checkPressureBoxes(placedObject);
             });
 
         } else {
-                // add a fourh pickup spot elswhere for swaps
+            // add a fourh pickup spot elswhere for swaps
             const box = new THREE.Mesh(
                 new THREE.BoxGeometry(2, 0.2, 2),
-                new THREE.MeshBasicMaterial({ color: '#FFA500' }) 
+                new THREE.MeshLambertMaterial({ color: 0xFFA500 }) 
             );
             box.position.set(-10, 0.1, 12);
             this.add(box);
@@ -509,13 +535,13 @@ export class OfficeConstruct extends Construct {
                 if ((placedObject === this.feather)){
                     placedObject.position.set(0,0.4,-0.7);
                     placedObject.scale.set(0.02,0.02,0.02); 
-                    }else if(placedObject == this.cauldron) {
+                } else if (placedObject == this.cauldron) {
                     placedObject.position.set(0,0.5,0);
                     placedObject.scale.set(0.04,0.04,0.04); 
-                    }else{
+                } else {
                     placedObject.position.set(0,1,3.4);
                     placedObject.scale.set(2.5,2.5,2.5) ;
-                    }
+                }
             });
             
         }
@@ -524,7 +550,6 @@ export class OfficeConstruct extends Construct {
 
     }
     update(): void {
-       //this.interactions.update();
     }
 
     destroy(): void {
