226911297e8abb419f285b6e2a9af59a58376434
Zach Schwark <zachs1708@gmail.com>
2023-10-16 13:08:39 +0200

    add credits

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -56,7 +56,7 @@ export class MainMenu extends Scene {
         const assets = [
             // { artist: '', title: '', type: '', license: '', link: '' },
             { artist: 'Nihal Ranchod', title: 'Chess Plinths', type: 'Model', license: 'None (made for project)', link: '' },
-            { artist: 'Brendan Griffihs', title: 'Lectern', type: 'Model', license: 'None (made for project)', link: '' },
+            { artist: 'Brendan Griffiths', title: 'Lectern', type: 'Model', license: 'None (made for project)', link: '' },
             { artist: 'flithycent', title: 'bookshelf', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/bookshelf-3c782b0787cc41f1b5974dbd7a1f8f53' },
             { artist: 'FlukierJupiter', title: 'Wooden Table', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/wooden-table-0dc1c7d6cbab4d74bef7c4f82abf2caf' },
             { artist: 'sergeilihandristov', title: 'Desk lamp', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/desk-lamp-ac5135b505694287a64b4370ea2cda8d' },
@@ -72,6 +72,14 @@ export class MainMenu extends Scene {
             { artist: 'Kowalo', title: 'Lute', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/lute-42e097aebea143b69beb9276ffd1cbc2' },
             { artist: 'Wersaus33', title: 'Classical Guitar - PS1 Low Poly', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/classical-guitar-ps1-low-poly-c0ae96b52bed4f5791cf6cea9f213694' },
             { artist: 'MarisDani', title: 'Djembe', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/djembe-9912df2ecc494753b9f08374c21a236f' },
+            { artist: 'Brendan Griffiths', title: 'WallLight', type: 'Model', license: 'None (made for project)', link: '' },
+            { artist: 'FlukierJupiter', title: 'Wooden Table', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/wooden-table-0dc1c7d6cbab4d74bef7c4f82abf2caf' },
+            { artist: 'filthycent', title: 'bookshelf', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/bookshelf-3c782b0787cc41f1b5974dbd7a1f8f53' },
+            { artist: 'sergeilihandristov', title: 'Desk lamp', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/desk-lamp-ac5135b505694287a64b4370ea2cda8d' },
+            { artist: 'ToneySK', title: 'chandelier', type: 'Model', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/chandelier-2a46291356fd432292223852da25c071' },
+            { artist: 'chrisg4919', title: 'Hogwarts Wood Paneling Material', type: 'Material', license: 'CC-BY-4.0', link: 'https://sketchfab.com/3d-models/hogwarts-wood-paneling-material-75680089a1d54680b951a10c570f99e9' },
+            { artist: 'Katsukagi', title: 'Wood Ceiling Coffers 001', type: 'Material', license: 'CC0', link: 'https://3dtextures.me/2020/03/11/wood-ceiling-coffers-001/' },
+            { artist: 'Katsukagi', title: 'Stone Floor 001', type: 'Material', license: 'CC0', link: 'https://3dtextures.me/2016/08/27/stone-floor-001/' },
         ];
         drawMainMenu(this.userInterface, developers, assets);
     }
